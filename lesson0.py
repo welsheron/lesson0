ab8608d6dca5a308d92be7112a96c82a15d19df3
welsh <48034117+welsheron@users.noreply.github.com>
2024-10-01 17:30:46 +0300

    Add files via upload

---
+++ lesson0.py
@@ -0,0 +1,14 @@
+# 1st program
+print(9**0.5*5)
+# 2nd program
+print(9.99>9.98 and 1000!=1000.1)
+# 3rd program
+a = print(2*2+2)
+b = print(2*(2+2))
+print(2*2+2==2*(2+2))
+print(a==b)
+# 4th program
+float(123.456)
+print(int(123.456*10)-1230)
+
+
